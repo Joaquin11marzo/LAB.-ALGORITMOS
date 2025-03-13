7490a16ea69ae002186c635b64b20423f2e6e68b
Joaquin <joaquin11marzo@gmail.com>
2025-03-13 15:23:21 -0300

    Commit de la clase 3

---
+++ clase 3/eje3.py
@@ -0,0 +1,3 @@
+nombre = input(str("Cual es tu nombre:"))
+print(f"Tu nombre con mayusucla es:{nombre.capitalize()}")#Cambia todas las letras del nombre elegido a minúsculas y cambia la primera a mayúsculas
+print(f"Tu nombre en minusculas es:{nombre.casefold()}")#Cambia todas las letras del nombre elegido a minúsculas
